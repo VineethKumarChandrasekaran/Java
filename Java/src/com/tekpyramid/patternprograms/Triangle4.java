23645848d93c5bffc19fe2d6c85a70100885768c
VineethKumar Chandrasekaran <vineethkumar2797@gmail.com>
2025-02-12 12:41:32 +0530

    Update

--- Java/src/com/tekpyramid/patternprograms/Triangle4.java
+++ Java/src/com/tekpyramid/patternprograms/Triangle4.java
@@ -1,19 +1,14 @@
 package com.tekpyramid.patternprograms;
 
 public class Triangle4 {
-	public static void main(String[] args) {
-		int row =5;
-		
-		for(int i=0; i<row; i++) {
-			for(int j=0; j<row; j++) {
-				if(i+j<=row+1) {
-					System.out.print("*");
-				}else {
-					System.out.print(" ");
-				}
-			}
-			System.out.println();
-		}
+    public static void main(String[] args) {
+        int row = 5; 
 
-	}
+        for (int i = row; i >= 1; i--) { 
+            for (int j = 1; j <= i; j++) { 
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+    }
 }
